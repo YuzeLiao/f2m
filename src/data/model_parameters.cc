e25c2066cc40789ccaaef0af3a70cb14285cc226
aksnzhy <mctt90@gmail.com>
2016-10-20 20:06:53 +0800

    update

---
+++ src/data/model_parameters.cc
@@ -0,0 +1,83 @@
+/* -------------------------------------------------------------------------- *
+ * Copyright (c) 2016 by contributors. All Rights Reserved.                   *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ *  Unless required by applicable law or agreed to in writing, software       *
+ *  distributed under the License is distributed on an "AS IS" BASIS,         *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *  See the License for the specific language governing permissions and       *
+ *  limitations under the License.                                            *
+ * -------------------------------------------------------------------------- */
+
+/*
+Author: Chao Ma (mctt90@gmail.com)
+
+This file is the implementation of model parameters.
+*/
+
+#include "src/data/model_parameters.h"
+
+namespace f2m {
+
+const real_t kInitMean = 0.0;
+const real_t kInitStdev = 0.01;
+
+Model::Model(index_t feature_num, ModelType type = LR,
+        int k = 0, index_t field_num = 0) :
+  m_feature_num(feature_num),
+  m_type(type),
+  m_k(k),
+  m_field_num(field_num) {
+  	CHECK_GT(m_feature_num, 0);
+    CHECK_GE(m_k, 0);
+    CHECK_GE(m_field_num, 0);
+    // allocate memory and initialize model parameters.
+    if (type == LR) {
+      // bias term + linear terms.
+      m_parameters_num = m_feature_num + 1;
+    } else if (type == FM) {
+      // bias term + linear terms + V
+      m_parameters_num = m_feature_num + 1 + 
+                         k * m_feature_num;
+    } else if (type == FFM) {
+      // bias term + linear terms + Matrix
+      m_parameters_num = m_feature_num + 1 + 
+                         k * m_field_num * m_feature_num;
+    } else {
+      LOG(FATAL) << "Unknow model type: " << type;
+    }
+    try {
+      m_parameters.resize(m_parameters_num);
+      InitModel();
+    } catch (std::bad_alloc&) {
+      LOG(FATAL) << "Cannot allocate enough memory for \
+                     current model parameters.";
+    }
+  }
+
+void Model::SaveFile() {
+
+}
+
+void Model::LoadFile() {
+
+}
+
+void Model::InitModel() {
+  // Init bias and linear terms.
+  for (index_t i = 0; i < m_feature_num + 1; ++i) {
+       m_parameters[i] = 0.0;
+  }
+  // Init V or Matrix
+  for (index_t i = m_feature_num + 1; i < m_parameters_num; ++i) {
+     // gaussian distribution
+     m_parameters[i] = ran_gaussian(kInitMean, kInitStdev);
+  }	
+}
+
+} // namespace f2m
--- src/data/model_parameters.h
+++ src/data/model_parameters.h
@@ -30,9 +30,6 @@ This file defines the model parameters used by f2m.
 
 namespace f2m {
 
-const real_t kInitMean = 0.0;
-const real_t kInitStdev = 0.01;
-
 enum ModelType { LR, FM, FFM };
 
 // Model is responsible for storing the global model prameters.
@@ -42,37 +39,13 @@ class Model {
  public:
   // By default, the constructor is used for LR.
   Model(index_t feature_num, ModelType type = LR,
-        int k = 0, index_t field_num = 0) :
-  m_feature_num(feature_num),
-  m_type(type),
-  m_k(k),
-  m_field_num(field_num) {
-    CHECK_GT(m_feature_num, 0);
-    CHECK_GE(m_k, 0);
-    CHECK_GE(m_field_num, 0);
-    // allocate memory and initialize model parameters.
-    if (type == LR) {
-      // bias term + linear terms.
-      m_parameters_num = m_feature_num + 1;
-    } else if (type == FM) {
-      // bias term + linear terms + V
-      m_parameters_num = m_feature_num + 1 + 
-                         k * m_feature_num;
-    } else if (type == FFM) {
-      // bias term + linear terms + Matrix
-      m_parameters_num = m_feature_num + 1 + 
-                         k * m_field_num * m_feature_num;
-    } else {
-      LOG(FATAL) << "Unknow model type: " << type;
-    }
-    try {
-      m_parameters.resize(m_parameters_num);
-      InitModel();
-    } catch (std::bad_alloc&) {
-      LOG(FATAL) << "Cannot allocate enough memory for \
-                     current model parameters.";
-    }
-  }
+        int k = 0, index_t field_num = 0);
+
+  // Save model to disk file.
+  void SaveModel();
+
+  // Load model from disk file.
+  void LoadModel();
 
  private:
   ModelType m_type;              // enum { LR, FM, FFM };
@@ -82,17 +55,7 @@ class Model {
   int m_k;                       // factor size for FM and FFM.
   int m_field_num;               // number of field. (only for FFM).
 
-  void InitModel() {
-     // Init bias and linear terms.
-     for (index_t i = 0; i < m_feature_num + 1; ++i) {
-       m_parameters[i] = 0.0;
-     }
-     // Init V or Matrix
-     for (index_t i = m_feature_num + 1; i < m_parameters_num; ++i) {
-       // gaussian distribution
-       m_parameters[i] = ran_gaussian(kInitMean, kInitStdev);
-     }
-  }
+  void InitModel();
 
   DISALLOW_COPY_AND_ASSIGN(Model);
 };
